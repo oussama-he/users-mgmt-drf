c703b03d2684ed92a9766cf9b61684b1209f4983
oussama <oussama.hel@gmail.com>
2024-01-29 17:06:33 +0100

    install and config drf jwt

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 from users.views import UserViewSet
 
@@ -12,5 +14,7 @@ router.register(r'users', UserViewSet, basename='user')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
