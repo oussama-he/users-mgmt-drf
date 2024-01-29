fe28faffa975127b3d1faa9cb23bd64b95c48bf4
oussama <oussama.hel@gmail.com>
2024-01-29 17:07:49 +0100

    use JWT for authorization

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
+from users.permissions import IsAuthenticatedForManipulation
 from users.serializers import UserSerializer
 
 User = get_user_model()
@@ -10,4 +10,4 @@ User = get_user_model()
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedForManipulation]
